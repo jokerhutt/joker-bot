c05e959b67fd7ca0de9f3e9a4c4ad1375fe38c09
Dave <davidglogowski2003@gmail.com>
2026-01-10 23:33:22 +0100

    Add alembic and schemaj

--- joker_bot/web/models/user.py
+++ joker_bot/web/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BigInteger
+from sqlalchemy import BigInteger, String
 from sqlalchemy.orm import Mapped, mapped_column
 from joker_bot.web.models.base import Base
 
@@ -9,3 +9,5 @@ class User(Base):
     id: Mapped[int] = mapped_column(
         BigInteger, primary_key=True, doc="Discord user ID (snowflake)"
     )
+
+    tag: Mapped[str | None] = mapped_column(String, nullable=True)
