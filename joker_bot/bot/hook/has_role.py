c23788df2c8f4ec7743a38f0de2a9ba128df60cf
Dave <davidglogowski2003@gmail.com>
2026-01-12 20:09:35 +0100

    fix(roles): Switch to array of strings in role check

--- joker_bot/bot/hook/has_role.py
+++ joker_bot/bot/hook/has_role.py
@@ -1,16 +1,17 @@
 import lightbulb
 
 
-def use_has_role(ctx: lightbulb.Context, role_name: str) -> bool:
+def use_has_role(ctx: lightbulb.Context, role_names: set[str]) -> bool:
     member = ctx.member
     if member is None:
         return False
 
-    app = ctx.app
-
     for role_id in member.role_ids:
-        role = app.cache.get_role(role_id)
-        if role and role.name.lower() == role_name:
+        role = ctx.app.cache.get_role(role_id)
+        if not role:
+            continue
+
+        if role.name.lower() in role_names:
             return True
 
     return False
